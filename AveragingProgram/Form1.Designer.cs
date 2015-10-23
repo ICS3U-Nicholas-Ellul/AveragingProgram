c8973c33b9672fba8531efc65a2c95cbc7393577
pirates19 <nicholas.ellul@ocsbstudent.ca>
2015-10-23 13:29:14 -0400

    Not finished yet

---
+++ AveragingProgram/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace AveragingProgram
+{
+    partial class frmAverageCalculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mnuMenuStrip = new System.Windows.Forms.MenuStrip();
+            this.mniFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.mniExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.nudInput = new System.Windows.Forms.NumericUpDown();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.lblAverage = new System.Windows.Forms.Label();
+            this.lstListOfNumbers = new System.Windows.Forms.ListBox();
+            this.mnuMenuStrip.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudInput)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mnuMenuStrip
+            // 
+            this.mnuMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniFile});
+            this.mnuMenuStrip.Location = new System.Drawing.Point(0, 0);
+            this.mnuMenuStrip.Name = "mnuMenuStrip";
+            this.mnuMenuStrip.Size = new System.Drawing.Size(259, 24);
+            this.mnuMenuStrip.TabIndex = 0;
+            this.mnuMenuStrip.Text = "menuStrip1";
+            // 
+            // mniFile
+            // 
+            this.mniFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniExit});
+            this.mniFile.Name = "mniFile";
+            this.mniFile.Size = new System.Drawing.Size(37, 20);
+            this.mniFile.Text = "File";
+            // 
+            // mniExit
+            // 
+            this.mniExit.Name = "mniExit";
+            this.mniExit.Size = new System.Drawing.Size(152, 22);
+            this.mniExit.Text = "Exit";
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInfo.Location = new System.Drawing.Point(6, 24);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(247, 40);
+            this.lblInfo.TabIndex = 2;
+            this.lblInfo.Text = "Enter number between 0-100.\r\nEnter -1 to quit.";
+            this.lblInfo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // nudInput
+            // 
+            this.nudInput.Location = new System.Drawing.Point(77, 81);
+            this.nudInput.Name = "nudInput";
+            this.nudInput.Size = new System.Drawing.Size(120, 20);
+            this.nudInput.TabIndex = 3;
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(90, 124);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(75, 23);
+            this.btnGo.TabIndex = 4;
+            this.btnGo.Text = "button1";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // lblAverage
+            // 
+            this.lblAverage.AutoSize = true;
+            this.lblAverage.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAverage.Location = new System.Drawing.Point(6, 263);
+            this.lblAverage.Name = "lblAverage";
+            this.lblAverage.Size = new System.Drawing.Size(145, 24);
+            this.lblAverage.TabIndex = 5;
+            this.lblAverage.Text = "The average is: ";
+            // 
+            // lstListOfNumbers
+            // 
+            this.lstListOfNumbers.FormattingEnabled = true;
+            this.lstListOfNumbers.Location = new System.Drawing.Point(77, 166);
+            this.lstListOfNumbers.Name = "lstListOfNumbers";
+            this.lstListOfNumbers.Size = new System.Drawing.Size(120, 82);
+            this.lstListOfNumbers.TabIndex = 6;
+            // 
+            // frmAverageCalculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(259, 336);
+            this.Controls.Add(this.lstListOfNumbers);
+            this.Controls.Add(this.lblAverage);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.nudInput);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.mnuMenuStrip);
+            this.MainMenuStrip = this.mnuMenuStrip;
+            this.Name = "frmAverageCalculator";
+            this.Text = "Form1";
+            this.mnuMenuStrip.ResumeLayout(false);
+            this.mnuMenuStrip.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudInput)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip mnuMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem mniFile;
+        private System.Windows.Forms.ToolStripMenuItem mniExit;
+        private System.Windows.Forms.Label lblInfo;
+        private System.Windows.Forms.NumericUpDown nudInput;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.Label lblAverage;
+        private System.Windows.Forms.ListBox lstListOfNumbers;
+    }
+}
+
---
+++ AveragingProgram/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AveragingProgram
+{
+    public partial class frmAverageCalculator : Form
+    {
+        int counter = 1;
+        decimal totalNumber = 0;
+
+        public frmAverageCalculator()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            decimal inputNumber = this.nudInput.Value;
+            decimal average;
+
+            if( inputNumber != -1)
+            {
+                this.lstListOfNumbers.Items.Add(inputNumber);
+                totalNumber = totalNumber + inputNumber;
+                average = totalNumber / counter;
+                counter++;
+
+                this.lblAverage.Text = "The average is: " + average;
+            }
+        }
+ 
+    }
+          public decimal RoundNumbers(decimal a,int b)
+    {
+
+    }
+}
